2eb8af53789f490357e9c1ea0f88395a29828c0a
Anna Tymchenko <git.user.ATymchenko@inno.tech>
2025-08-24 21:01:58 +0300

    Не удается обновить через Intellij IDEA

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-            System.out.println("Hello world!");
-            System.out.println("Git из IE");
-            System.out.println("11111111111111");
+        System.out.println("Hello world!");
+        System.out.println("Git из IE");
+        System.out.println("11111111111111");
     }
 }
