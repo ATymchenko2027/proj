536cab966c4a89baf61c1fd035269d489994fb77
Anna Tymchenko <git.user.ATymchenko@inno.tech>
2025-08-24 21:19:49 +0300

    Без доступа коммит через GIT

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Git из IE");
         System.out.println("11111111111111");
+        System.out.println("1111111111112222222222211");
     }
 }
