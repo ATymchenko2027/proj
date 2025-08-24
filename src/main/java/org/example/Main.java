cce4ffbed8093a6fe25351981b10c7a485d4e622
ATymchenko2027 <ATymchenko@inno.tech>
2025-08-24 21:23:12 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("Git из IE");
         System.out.println("11111111111111");
         System.out.println("1111111111112222222222211");
+        System.out.println("111111111111233333333333333333222222222211");
     }
 }
